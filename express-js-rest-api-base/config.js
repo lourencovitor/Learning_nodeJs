1233ceb85f75d75ad34ab0c10c53f0e320a002db
Vitor Lourenço <50245199+lourencovitor@users.noreply.github.com>
2020-08-09 22:30:12 -0300

    refactoring sensitive data

---
+++ express-js-rest-api-base/config.js
@@ -0,0 +1,10 @@
+const config = {
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "usertest",
+  },
+};
+
+module.exports = config;
--- express-js-rest-api-base/database/connection.js
+++ express-js-rest-api-base/database/connection.js
@@ -1,11 +1,12 @@
-var knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
-    }
-  });
+const { connection } = require("../config");
+var knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: connection.host,
+    user: connection.user,
+    password: connection.password,
+    database: connection.database,
+  },
+});
 
-module.exports = knex
+module.exports = knex;
